e46064d3bfd6fd882089049247c975b2fefee2a8
cmondragon15117 <cmondragon15117@gmail.com>
2018-02-22 02:13:05 -0600

    Added quad movement and camera changes
    Quads can now move manually in xyz plane. Cannot rotate. Cameras can now be changed to show various views.

--- UnityQuadSim/Assets/QuadMotor.cs
+++ UnityQuadSim/Assets/QuadMotor.cs
@@ -4,13 +4,35 @@ using UnityEngine;
 
 public class QuadMotor : MonoBehaviour {
 
+	private Vector3 veloctiy = Vector3.zero;
+
+	private Rigidbody rb;
 	// Use this for initialization
 	void Start () {
-		
+		rb = GetComponent<Rigidbody> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+	//Gets a movement vector
+	public void move (Vector3 _velocity) {
+		veloctiy = _velocity; 
+	}
+
+	void FixedUpdate() {
+		PerformMovement ();
+
+	}
+
+
+	void PerformMovement() {
+		if (veloctiy != Vector3.zero) {
+			rb.MovePosition (rb.position + veloctiy * Time.fixedDeltaTime);
+		}
+	}
+
+
 }
