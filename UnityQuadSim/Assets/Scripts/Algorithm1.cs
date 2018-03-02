ed28822cf88d535e444cf96957c5f3599ff7d625
Hasan Sehwail <hsehwail17@gmail.com>
2018-03-02 00:05:58 -0600

    added first algoithm
    I made the robots move autonomously, so when I make the algoithms, they would search on their own like they will in the competition . Also, I added the first algorithm. It's pretty basic and I put the description of how the algorithm works on a doc on our google drive called "algorithms". It is pretty basic and there are some things that I still need to clear up using this algorithm but so far it works. I  haven't tested it with any other algoithm, I just wanted a basic algorithm that worked.

---
+++ UnityQuadSim/Assets/Scripts/Algorithm1.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Algorithm1 : MonoBehaviour {
+    
+
+    public float movementSpeed = 5.0f;
+    public float clockWise = 1000.0f;
+    public float counterClockWise = -5.0f;
+    private int counter = 0;
+    private int counter2 = 0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // move forward
+        if (transform.position.x > -16 && (counter2+4)%4 == 0)
+        {
+            transform.position += Vector3.left * Time.deltaTime * movementSpeed;
+            Debug.Log(counter2);
+        }
+
+        if (transform.position.x < -16)
+        {
+            counter2++;
+        }
+        // move to the side
+        if ((counter2+4)%4 == 1)
+        {
+            transform.Translate(0.0f, 0.0f, -1.0f);
+            counter2++;
+        }
+        // move back
+        if (transform.position.x < 22 && (counter2+4)%4 ==2)
+        {
+            transform.position += -Vector3.left * Time.deltaTime * movementSpeed;
+            Debug.Log(counter2);
+        }
+
+        if (transform.position.x >= 22 & (counter2+4)%4 == 2)
+        {
+            counter2++;
+        }
+        // move to the side
+        if ((counter2+4)%4 == 3)
+        {
+            transform.Translate(0.0f, 0.0f, -1.0f);
+            counter2++;
+        }
+        
+        //transform.position += Vector3.left * Time.deltaTime * 0;
+            //transform.position += -transform.forward * Time.deltaTime * movementSpeed;
+           
+        /*while (transform.position.x < 0)
+        {
+            //transform.position += -Vector3.left * Time.deltaTime * movementSpeed;
+            /*while (counter == 0)
+            {
+                transform.Translate(0.0f, 0.0f, -1.0f);
+                
+                counter++;
+            }*/
+            /*transform.position += -Vector3.left * Time.deltaTime * movementSpeed;
+
+        }*/
+    }
+
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "obstacle")
+        {
+            Debug.Log("We hit an obstacle!");
+            transform.Translate(0.0f, 0.0f, 0.0f);
+            transform.position += Vector3.left * Time.deltaTime * 0;
+        }
+    }
+
+}
---
+++ UnityQuadSim/Assets/Scripts/randomMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class randomMovement : MonoBehaviour {
+    /*
+     IGNORE THIS SCRIPT
+     */
+    void Start()
+    {
+        InvokeRepeating("SetRandomPos", 0, 1);
+    }
+
+    void SetRandomPos()
+    {
+        Vector3 temp = new Vector3(Random.Range(-19.0f, 19.0f), Random.Range(-2.0f, -2.0f), Random.Range(-8.5f, 8.5f));
+        transform.position = temp;
+    }
+
+    private void FixedUpdate()
+    {
+        SetRandomPos();
+    }
+}
