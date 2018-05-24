1ba7ad882624dcba2014bb6bc023bea06d6b62f5
Crista <cmondragon15117@gmail.com>
2018-05-24 15:52:02 -0500

    serial-comm updated for 1 external sensor
    serial-comm has added testing code for flight
    initial tests passed

--- AR Drone Code/serial-comm.js
+++ AR Drone Code/serial-comm.js
@@ -1,6 +1,14 @@
 // $ node serial-comm.js COM#
 // find the COM number that the arduino is connected to and type COM# after the node command
 
+
+var arDrone = require('ar-drone');
+var client  = arDrone.createClient();
+
+client.takeoff();
+
+
+
 var serialport = require('serialport');
 var portName = process.argv[2];
 
@@ -14,8 +22,17 @@ myPort.on('data', onData);
 
 function onOpen(){
     console.log('Open connection: ' + portName);
+    data = 10;
 }
 
 function onData(data){
     console.log('On Data ' + data);
+    client.stop();
+    if(data < 10 && data != 0) {
+    	console.log('COLLISION WILL HAPPEN');
+    	client.front(.2);
+    	client.after(3000, function() { //delay in ms
+				client.stop();
+			});
+    }
 }
