d70ad52779ba6b5b1f11404775b8fff2fed151b2
Hasan Sehwail <hsehwail17@gmail.com>
2018-06-19 21:52:21 -0500

    changed drone web server and text to speech

--- AR Drone Code/Speech/TextToSpeechService.js
+++ AR Drone Code/Speech/TextToSpeechService.js
@@ -1,12 +1,13 @@
+// hasan version
 var request = require('request'),
     xmlbuilder = require('xmlbuilder'),
     wav = require('wav'),
     Speaker = require('speaker');
 
-exports.Synthesize = function Synthesize(text){
+exports.Synthesize = function Synthesize(text) {
 
-    
-    var apiKey = "653b3d6e08484f639ddf4b565adf3a3f";
+
+    var apiKey = "f070e23e6b794b51bc2b1065763ee241";
     var ssml_doc = xmlbuilder.create('speak')
         .att('version', '1.0')
         .att('xml:lang', 'en-us')
@@ -19,9 +20,9 @@ exports.Synthesize = function Synthesize(text){
     var post_speak_data = ssml_doc.toString();
 
     request.post({
-    	url: 'https://api.cognitive.microsoft.com/sts/v1.0/issueToken',
+        url: 'https://api.cognitive.microsoft.com/sts/v1.0/issueToken',
         headers: {
-            'Ocp-Apim-Subscription-Key' : apiKey
+            'Ocp-Apim-Subscription-Key': apiKey
         }
     }, function (err, resp, access_token) {
         if (err || resp.statusCode != 200) {
@@ -32,8 +33,8 @@ exports.Synthesize = function Synthesize(text){
                     url: 'https://speech.platform.bing.com/synthesize',
                     body: post_speak_data,
                     headers: {
-                        'content-type' : 'application/ssml+xml',
-                        'X-Microsoft-OutputFormat' : 'riff-16khz-16bit-mono-pcm',
+                        'content-type': 'application/ssml+xml',
+                        'X-Microsoft-OutputFormat': 'riff-16khz-16bit-mono-pcm',
                         'Authorization': 'Bearer ' + access_token,
                         'X-Search-AppId': '07D3234E49CE426DAA29772419F436CA',
                         'X-Search-ClientID': '1ECFAE91408841A480F00935DC390960',
--- AR Drone Code/drone-web-server.js
+++ AR Drone Code/drone-web-server.js
@@ -1,85 +1,133 @@
 /* Web server code for drone, used for website and autonomous flying */
 var express = require('express');
 var arDrone = require('ar-drone'); // converts parrot sdk to node js
-var http    = require('http');
+var http = require('http');
 var client = arDrone.createClient();
 var app = express();
 app.use(express.static('public'));
 var path = require('path');
 var fs = require('fs');
+var nameToFind = "";
 
 app.get('/', function (req, res) {
- res.sendFile(path.join(__dirname + '/index.html'));
+    res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.get('/land', function(req, res) {
+app.get('/land', function (req, res) {
 
- client.stop(0);
- client.land();
- 
- console.log("Drone is now landing");
+    client.stop(0);
+    client.land();
+
+    console.log("Drone is now landing");
 });
 
-app.get('/takeoff', function(req, res) {
- console.log("Drone Taking Off!");
- 
- client.takeoff();
- 
+app.get('/takeoff', function (req, res) {
+    console.log("Drone Taking Off!");
+
+    client.takeoff();
+
 });
 
-app.get('/calibrate',function(req,res){
- client.calibrate(0);
- console.log("Drone Calibrating! Stand Clear!");
+app.get('/calibrate', function (req, res) {
+    client.calibrate(0);
+    console.log("Drone Calibrating! Stand Clear!");
 });
-app.get('/flip',function(req,res){
+app.get('/flip', function (req, res) {
 
- client.animate('flipAhead', 1000);
- console.log("Flip Forward!");
+    client.animate('flipAhead', 1000);
+    console.log("Flip Forward!");
 });
-app.get('/spin',function(req,res){
- client.calibrate(0);
- console.log("Drone Rotating Clockwise Stand Clear!");
+app.get('/spin', function (req, res) {
+    client.calibrate(0);
+    console.log("Drone Rotating Clockwise Stand Clear!");
 });
-app.get('/camerabottom',function(req,res){
- client.config('video:video_channel', 3);
- console.log("pointing below");
+app.get('/camerabottom', function (req, res) {
+    client.config('video:video_channel', 3);
+    console.log("pointing below");
 });
-app.get('/cameraforward',function(req,res){
- client.config('video:video_channel', 0);
+app.get('/cameraforward', function (req, res) {
+    client.config('video:video_channel', 0);
 
- console.log("pointing forward");
+    console.log("pointing forward");
 });
 
 
-app.get('/photos', function(req, res) {
-   console.log("Drone Taking Pictures");
-      
-   var pngStream = client.getPngStream();
-   var period = 2000; // Save a frame every 2000 ms.
-   var lastFrameTime = 0;
-   pngStream
-     .on('error', console.log)
-     .on('data', function(pngBuffer) {
-        var now = (new Date()).getTime();
-        if (now - lastFrameTime > period) {
-           lastFrameTime = now;
-           fs.writeFile(__dirname + '/public/DroneImage.png', pngBuffer, function(err) {
-           if (err) {
-             console.log("Error saving PNG: " + err);
-           } else {
-             console.log("Saved Frame");  
+app.get('/photos', function (req, res) {
+    console.log("Drone Taking Pictures");
+
+    var pngStream = client.getPngStream();
+    var period = 2000; // Save a frame every 2000 ms.
+    var lastFrameTime = 0;
+    pngStream
+      .on('error', console.log)
+      .on('data', function (pngBuffer) {
+          var now = (new Date()).getTime();
+          if (now - lastFrameTime > period) {
+              lastFrameTime = now;
+              fs.writeFile(__dirname + '/public/DroneImage.png', pngBuffer, function (err) {
+                  if (err) {
+                      console.log("Error saving PNG: " + err);
+                  } else {
+                      console.log("Saved Frame");
+                  }
+              });
           }
       });
-     }
-  });
- });
+});
 
-app.get('/clockwise', function(req, res) {
- client.clockwise(0.5);
- console.log("Drone Turning Clockwise");
-}); 
+/*
+ This is a new router function in our file.
+ You can see EXACTLY what is sent to the back-end there is a console.log line 
+ which prints the value of the parameter inputspeech which is passed in with the request.  
+*/
+app.get('/audio', function (req, res) {
+    console.log(req.query.inputspeech);
+    nameToFind = req.query.inputspeech;
+    if (name == "hello") {
+        var tts = require('./TextToSpeechService.js');
+        tts.Synthesize("Hello");
+    }
+});
+
+
+
+app.get('/audio', function (req, res) {
+    console.log(req.query.inputspeech);
+    nameToFind = req.query.inputspeech;
+    console.log("Drone Taking Off");
+    client.takeoff();
+    console.log("Drone Taking Pictures");
+    var pngStream = client.getPngStream();
+    var period = 5000; // Save a frame every 5000 ms.
+    var lastFrameTime = 0;
+    pngStream
+     .on('error', console.log)
+     .on('data', function (pngBuffer) {
+         var now = (new Date()).getTime();
+         if (now - lastFrameTime > period && found == false) {
+             lastFrameTime = now;
+             fs.writeFile(__dirname + '/Public/DroneImage.png', pngBuffer, function (err) {
+                 if (err) {
+                     console.log("Error saving PNG: " + err);
+                 } else {
+                     console.log("Saved Frame");
+                     mapPersonList(myGroup);
+                     identifyPATH(__dirname + '/Public/DroneImage.png', myGroup);
+                 }
+             });
+         }
+     });
+});
+
+
+
+app.get('/clockwise', function (req, res) {
+    client.clockwise(0.5);
+    console.log("Drone Turning Clockwise");
+});
 
 
 
 app.listen(3000, function () {
 });
+
